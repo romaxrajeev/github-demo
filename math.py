cd9f52d0d2f935b7bc259020e0020cb9c08f1d3f
Romax Rajeev <64697574+romeo611199@users.noreply.github.com>
2021-07-02 14:20:52 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#Add implementation
+def add(x,y):
+    return x + y
+#Subtract implementation
+def subtract(x,y):
+    return  x - y   #master
+#Multiply implementation
+def multiply(x,y):
+    return x * y
+#Divide implementation
+def divide(x,y):
+    if y == 0: return ZeroDivisionError
+    return x / y    #bug789
+
+def square(x):
+    pass
